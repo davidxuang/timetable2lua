e07180c1998ada0a93e53a3a17f96757f5b83212
David Huang <davidxuang@live.com>
2024-06-08 10:46:28 +0800

    chore: Release v0.1.3

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,17 @@ export default defineConfig({
   plugins: [
     monkey({
       entry: 'src/main.ts',
+      build: {
+        metaFileName: true,
+      },
       userscript: {
         icon: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji/assets/Metro/3D/metro_3d.png',
         namespace: 'https://dvxg.de/',
         match: 'https://www.cqmetro.cn/smbsj.html',
+        updateURL:
+          'https://github.com/davidxuang/timetable2lua/releases/latest/download/timetable2lua.meta.js',
+        downloadURL:
+          'https://github.com/davidxuang/timetable2lua/releases/latest/download/timetable2lua.user.js',
       },
     }),
   ],
